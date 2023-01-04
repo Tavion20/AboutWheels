8140a2e8260816b92e5d8d9f51e832a7dd76572d
Tavion20 <tavionalric@gmail.com>
2023-01-04 17:49:33 +0530

    Added Functionality

---
+++ index.js
@@ -0,0 +1,64 @@
+function changepolo(){
+    document.getElementById("mainimage").src="logo/vwpolo.jpg";
+    document.getElementById("text").innerHTML="Polo Text";
+}
+function changeaven(){
+    document.getElementById("mainimage").src="logo/lambo.webp";
+    document.getElementById("text").innerHTML="Lamborghini Aventador";
+}
+function changehura(){
+    document.getElementById("mainimage").src="logo/huri.webp";
+    document.getElementById("text").innerHTML="Lamborghini Huracan";
+}
+function changeurus(){
+    document.getElementById("mainimage").src="logo/urus.webp";
+    document.getElementById("text").innerHTML="Lamborghini Urus";
+}
+function changevantc(){
+    document.getElementById("mainimage").src="logo/vantc.jpg";
+    document.getElementById("text").innerHTML="Vantage Coupe";
+}
+function changevantr(){
+    document.getElementById("mainimage").src="logo/vantr.webp";
+    document.getElementById("text").innerHTML="Vantage Roadster";
+}
+function changedbx(){
+    document.getElementById("mainimage").src="logo/dbx.jpg";
+    document.getElementById("text").innerHTML="DBX707";
+}
+function changedbs(){
+    document.getElementById("mainimage").src="logo/dbs.webp";
+    document.getElementById("text").innerHTML="DBS";
+}
+function changedb11(){
+    document.getElementById("mainimage").src="logo/db11.jpg";
+    document.getElementById("text").innerHTML="DB11";
+}
+function changedbsv(){
+    document.getElementById("mainimage").src="logo/dbsv.jpg";
+    document.getElementById("text").innerHTML="DBS Volante";
+}
+function changetay(){
+    document.getElementById("mainimage").src="logo/taycan.jpg";
+    document.getElementById("text").innerHTML="Taycan";
+}
+function change718(){
+    document.getElementById("mainimage").src="logo/718.jpeg";
+    document.getElementById("text").innerHTML="718";
+}
+function change911(){
+    document.getElementById("mainimage").src="logo/911.webp";
+    document.getElementById("text").innerHTML="911";
+}
+function changemac(){
+    document.getElementById("mainimage").src="logo/macan.jpg";
+    document.getElementById("text").innerHTML="Macan";
+}
+function changecay(){
+    document.getElementById("mainimage").src="logo/cay.jpg";
+    document.getElementById("text").innerHTML="Cayenne";
+}
+function changepan(){
+    document.getElementById("mainimage").src="logo/pan.jpg";
+    document.getElementById("text").innerHTML="Panamera";
+}
