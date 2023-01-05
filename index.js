2badd136f911ff6c22cba9618d666090abfd0e8b
jackfr8st <86789457+jackfr8st@users.noreply.github.com>
2023-01-05 17:16:03 +0530

    added renault and hyundai pages

--- index.js
+++ index.js
@@ -234,3 +234,67 @@ function changebrezza(){
     document.getElementById("speed").innerHTML="Top speed: 170 km/h";
     document.getElementById("type").innerHTML="Type: compact SUV";
 }
+
+//hyundai
+
+function changeverna(){
+    document.getElementById("mainimage").src="logo/verna.jpg";
+    document.getElementById("model").innerHTML="Model: Verna"; 
+    document.getElementById("engsp").innerHTML="Engine Specs: 1.5L 4-cylinder 1497 cc";
+    document.getElementById("mile").innerHTML="Mileage: 17.7 to 25.0 km/L";
+    document.getElementById("price").innerHTML="Base Price: ₹9.44 Lakh";
+    document.getElementById("speed").innerHTML="Top speed: 203 Km/hr";
+    document.getElementById("type").innerHTML="Type: Sedan";
+}
+
+function changecreta(){
+    document.getElementById("mainimage").src="logo/creta.jpg";
+    document.getElementById("model").innerHTML="Model: Creta"; 
+    document.getElementById("engsp").innerHTML="Engine specs: 1.5L 3-cylinder 1493 cc";
+    document.getElementById("mile").innerHTML="Mileage: 16.8 km/L";
+    document.getElementById("price").innerHTML="Base Price: ₹10.44 Lakh";
+    document.getElementById("speed").innerHTML="Top speed: 195 km/h";
+    document.getElementById("type").innerHTML="Type: SUV";
+}
+
+function changekona(){
+    document.getElementById("mainimage").src="logo/kona.jpg";
+    document.getElementById("model").innerHTML="Model: Kona electric"; 
+    document.getElementById("engsp").innerHTML="Battery: 39.2kWh";
+    document.getElementById("mile").innerHTML="Electric range: 452 km/full charge";
+    document.getElementById("price").innerHTML="Base Price: ₹23.84 Lakh";
+    document.getElementById("speed").innerHTML="Top speed: 	167 km/h";
+    document.getElementById("type").innerHTML="Type: EV";
+}
+
+//renault
+
+function changekwid(){
+    document.getElementById("mainimage").src="logo/kwid.jpg";
+    document.getElementById("model").innerHTML="Model: KWID"; 
+    document.getElementById("engsp").innerHTML="Engine Specs: 1.0L 3-cylinder 999 cc";
+    document.getElementById("mile").innerHTML="Mileage: 22.0 to 22.25 km/L";
+    document.getElementById("price").innerHTML="Base Price: ₹4.70 Lakh";
+    document.getElementById("speed").innerHTML="Top speed: 130Km/hr";
+    document.getElementById("type").innerHTML="Type: Hatchback";
+}
+
+function changekiger(){
+    document.getElementById("mainimage").src="logo/kiger.jpg";
+    document.getElementById("model").innerHTML="Model: Kiger"; 
+    document.getElementById("engsp").innerHTML="Engine specs: 1.0L 3-cylinder 999 cc";
+    document.getElementById("mile").innerHTML="Mileage: 18.24 to 20.5 km/L";
+    document.getElementById("price").innerHTML="Base Price: ₹6.00 Lakh";
+    document.getElementById("speed").innerHTML="Top speed: 155 km/h";
+    document.getElementById("type").innerHTML="Type: subcompact SUV";
+}
+
+function changetriber(){
+    document.getElementById("mainimage").src="logo/triber.jpg";
+    document.getElementById("model").innerHTML="Model: Triber"; 
+    document.getElementById("engsp").innerHTML="Engine specs: 1.0L 3-cylinder 1063 cc";
+    document.getElementById("mile").innerHTML="Mileage: 20.0 km/L";
+    document.getElementById("price").innerHTML="Base Price: ₹6.00 Lakh";
+    document.getElementById("speed").innerHTML="Top speed: 140 km/h";
+    document.getElementById("type").innerHTML="Type: Hatchback";
+}
